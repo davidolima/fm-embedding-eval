167f912430d9ee3b211053be5a2fd3e701bca48a
David <davidlima@ufba.br>
2025-04-16 10:53:06 -0300

    refact: Refactor model test suite

--- utils/test_models.py
+++ utils/test_models.py
@@ -2,46 +2,26 @@ import torch
 
 from huggingface_hub import login
 
-from models import UNI, UNI2, Phikon, PhikonV2
+from models import UNI, UNI2, Phikon, PhikonV2, MAE
 
 test_input = torch.rand((1,3,224,224))
 
-def test_wrapper(func):
-    def wrapper():
-        print(f"----- {func.__name__} -----")
-        print("Input shape:", test_input.shape)
-        feats_shape = func()
-        print("Output shape:", feats_shape)
+MODELS = (
+    UNI,
+    UNI2,
+    Phikon,
+    PhikonV2,
+    MAE,
+)
 
-    return wrapper
-
-@test_wrapper
-def test_uni2():
-    model = UNI2()
+def test_wrapper(model):
+    print(f"----- {model.name} -----")
+    print("Input shape:", test_input.shape)
+    model = model(device='cpu')
     feats = model(test_input)
-    return feats.shape
-
-@test_wrapper
-def test_phikonv2():
-    model = PhikonV2()
-    feats = model(test_input)
-    return feats.shape
-
-@test_wrapper
-def test_phikon():
-    model = Phikon()
-    feats = model(test_input)
-    return feats.shape
-
-@test_wrapper
-def test_uni():
-    model = UNI()
-    feats = model(test_input)
-    return feats.shape
+    print("Output shape:", feats.shape)
        
 if __name__ == '__main__':
-    test_uni()
-    test_uni2()
-    test_phikon()
-    test_phikonv2()
+    for model in MODELS:
+        test_wrapper(model)
     print("----- END OF TESTS -----")
