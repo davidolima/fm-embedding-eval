619878d6ed92eb9a3d4832d86c6be9365eb3eb24
davidolima <davidlima@ufba.br>
2025-05-06 13:24:38 -0300

    fix: Class identification problem when loading data

--- data/glomerulus.py
+++ data/glomerulus.py
@@ -29,35 +29,86 @@ class GlomerulusDataset(Dataset):
 
         self.root = root_dir
         self.transforms = transforms
-        self.classes = classes if classes is not None else \
-                ["Membranous", "Sclerosis", "Crescent", "Normal", "Hypercelularidade", "Podocitopatia"]
-        
+
+        if classes:
+            self.classes = classes
+        else:
+            # Folders found in root_dir will be used as classes
+            self.classes = [x for x in os.listdir(root_dir) if os.path.isdir(os.path.join(root_dir, x))]
+            print(f"[!] Classes auto-detected: {self.classes}")
+
         self.data = GlomerulusDataset.load_data(self.root, self.classes)
 
     @staticmethod
-    def identify_class_folders(root: str, classes: list[str]) -> dict[str, list[str]]:
+    def get_image_from_folders(folder_path:str) -> list[str]:
+        """
+        Get all images from a folder.
+        """
+        if os.path.isfile(folder_path) or os.listdir(folder_path) == []:
+            return []
+
+        images = []
+        for path, subfolders, files in os.walk(folder_path):
+            # Get all files in the folder
+            images.extend([os.path.join(path, x) for x in files if x.endswith(tuple(Config.SUPPORTED_IMAGE_TYPES))])
+            for subfolder in subfolders:
+                images.extend(GlomerulusDataset.get_image_from_folders(os.path.join(path, subfolder)))
+
+        return images
+
+    @staticmethod
+    def iterate_over_class_images(root: str, classes: list[str]) -> dict[str, list[str]]:
+        """
+        Iterate over all classes and get all images from each class.
+        """
+
+        # Get all folders containing <class> in their name
         folders_per_class = dict.fromkeys(classes)
         for c in classes:
             folders_per_class[c] = [x for x in os.listdir(root) if c in x]
-        return folders_per_class
 
-    @staticmethod
-    def load_data(root_dir: str, classes: list[str]) -> list[Tuple[str, int]]:
-        class_folders = GlomerulusDataset.identify_class_folders(root_dir, classes)
-
-        data = []
-        for c, folders in class_folders.items():
+        # Get all images from each folder
+        for c, folders in folders_per_class.items():
             class_images = []
             for folder in folders:
-                folder_images = sum([glob.glob(os.path.join(root_dir, folder, f"*.{ext}")) for ext in Config.SUPPORTED_IMAGE_TYPES], [])
-                folder_images = [os.path.join(root_dir, folder, x) for x in folder_images]
-                class_images.extend(folder_images)
-                
-            class_images = [(x, classes.index(c)) for x in class_images]
+                images = GlomerulusDataset.get_image_from_folders(os.path.join(root, folder))
+                class_images.extend(images)
+
+            yield c, class_images
+        
+    @staticmethod
+    def load_data(root_dir: str, classes: list[str]) -> list[Tuple[str, int]]:
+        data = []
+        for c, images in GlomerulusDataset.iterate_over_class_images(root_dir, classes):
+            class_images = [(x, classes.index(c)) for x in images]
             data.extend(class_images)
+
+            if c == 'Hypercelularidade':
+                print(images)
 
         print(f"[!] Dataset loaded. ({len(data)} images and {len(classes)} classes)")
         return data
+
+    def info(self) -> None:
+        """
+        Print dataset info.
+        """
+        print(f"Dataset: {self.__class__.__name__}")
+        print(f"Number of images: {len(self.data)}")
+        [print(f"  - {c}: {n}") for c, n in self.count_images_per_class().items()]
+        print(f"Number of classes: {len(self.classes)}")
+        print(f"Classes: {self.classes}")
+        print(f"Root directory: {self.root}")
+
+    def count_images_per_class(self) -> dict[str, int]:
+        """
+        Count the number of images per class.
+        """
+        count = {c: 0 for c in self.classes}
+        for _, label in self.data:
+            count[self.classes[label]] += 1
+        return count
+
 
     def __repr__(self) -> str:
         return f"<GlomerulusDataset with {self.__len__()} images and {len(self.classes)} classes>"
@@ -75,10 +126,11 @@ class GlomerulusDataset(Dataset):
         return (img, label, img_path)
 
 if __name__ == '__main__':
-    ds = GlomerulusDataset("/datasets/terumo-data-jpeg")
+    ds = GlomerulusDataset("/datasets/terumo-val")
     print(ds)
 
-    image, label = ds[0]
+    image, label, path = ds[0]
 
-    print(image)
-    print(label)
+    ds.info()
+    #print(image)
+    #print(label)
